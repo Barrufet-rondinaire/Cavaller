0eb02a7cef46cfb4cf03ae183fa307fa1e0dc4f3
Xavier Sala <utrescu@gmail.com>
2024-12-13 10:30:41 +0100

    feat: WIP Pintar imatge en la finestra

---
+++ MovimentImatge/Program.cs
@@ -0,0 +1,27 @@
+﻿using Heirloom;
+using Heirloom.Desktop;
+
+namespace MovimentImatge;
+
+class Program
+{
+    private static Window finestra;
+    private static Image cavaller;
+    
+    static void Main()
+    {
+        Application.Run(() =>
+        {
+            finestra = new Window("La finestra", (800, 600));
+            cavaller = new Image("imatges/cavaller.png");
+            var loop = GameLoop.Create(finestra.Graphics, OnUpdate);
+            loop.Start();
+        });
+    }
+
+    private static void OnUpdate(GraphicsContext gfx, float dt)
+    {
+        gfx.Clear(Color.Blue);
+        gfx.DrawImage(cavaller, new Vector(10,10));
+    }
+}
